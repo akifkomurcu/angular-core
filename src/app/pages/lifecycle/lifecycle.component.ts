6798152635915f8efaa3c928da4242612b61f49b
akifkomurcu <akif.tonmaister58@gmail.com>
2023-12-15 07:32:41 +0300

    data binding with ViewChild

--- src/app/pages/lifecycle/lifecycle.component.ts
+++ src/app/pages/lifecycle/lifecycle.component.ts
@@ -1,15 +1,25 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { SubPageComponent } from './sub-page/sub-page/sub-page.component';
 
 @Component({
   selector: 'app-lifecycle',
   templateUrl: './lifecycle.component.html',
   styleUrls: ['./lifecycle.component.css']
 })
-export class LifecycleComponent implements OnInit {
+export class LifecycleComponent implements OnInit, AfterViewInit {
 
   constructor() { }
-  ngOnInit() {
-  }
+  @ViewChild('childComponentRef', { static: false })
+  sub!: SubPageComponent;
 
+  ngOnInit() {
+    console.log('ngOnInit', this.sub);
+  }
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit', this.sub);
+    if(this.sub) {
+      this.sub.data = 'Veriyi Değiştirdim';
+    }
+  }
 
 }
